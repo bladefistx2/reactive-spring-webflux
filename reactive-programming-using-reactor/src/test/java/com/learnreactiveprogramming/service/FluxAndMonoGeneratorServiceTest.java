ce373bf0bbf0d8f2297b657e708c9a48e86790d6
Yoel <4094788+bladefistx2@users.noreply.github.com>
2022-07-20 15:29:09 +0300

    rename

--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,72 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class FluxAndMonoGeneratorServiceTest {
+public class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+    @Test
+    public void namesFlux(){
+        // given
+
+        // when
+        var namesFlux = fluxAndMonoGeneratorService.namesFlux();
+        // then
+        StepVerifier.create(namesFlux)
+//                .expectNext("James", "Mary", "Moses")
+                .expectNextCount(3)
+                .verifyComplete();
+    }
+
+    @Test
+    public void namesFluxMap() {
+        var namesFluxMap = fluxAndMonoGeneratorService.namesFluxMap(5);
+
+        StepVerifier.create(namesFluxMap)
+                .expectNext("5-JAMES", "5-MOSES")
+                .verifyComplete();
+    }
+
+    @Test
+    public void nameMono() {
+        var monoFlux = fluxAndMonoGeneratorService.nameMono();
+
+        StepVerifier.create(monoFlux)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    @Test
+    public void namesFluxFlatMap() {
+        var namesFluxFlatMap = fluxAndMonoGeneratorService.namesFluxFlatMap();
+
+        StepVerifier.create(namesFluxFlatMap)
+                .expectNext("J","A","M","E","S","M","A","R","Y","M","O","S","E","S")
+                .verifyComplete();
+    }
+
+
+    @Test
+    public void nameMonoFlatMap() {
+        var nameMonoFlat = fluxAndMonoGeneratorService.nameMonoFlatMap();
+
+        StepVerifier.create(nameMonoFlat)
+                .expectNext(List.of("J","E","S","U","S"))
+                .verifyComplete();
+    }
+
+    @Test
+    public void nameMonoFlatMapMany(){
+        var nameMonoMany = fluxAndMonoGeneratorService.nameMonoFlatMapMany();
+
+        StepVerifier.create(nameMonoMany)
+                .expectNext("J","E","S","U","S")
+                .verifyComplete();
+    }
 }
